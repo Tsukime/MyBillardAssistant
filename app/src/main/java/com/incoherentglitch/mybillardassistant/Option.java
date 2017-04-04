3afde0bf37f6ecbb2e42c749ce4a9a710f877048
Tsukime <keip.priscilla@outlook.fr>
2017-04-04 14:10:55 +0200

    options + text blanc regles americain

--- app/src/main/java/com/incoherentglitch/mybillardassistant/Option.java
+++ app/src/main/java/com/incoherentglitch/mybillardassistant/Option.java
@@ -4,46 +4,157 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class Option extends Activity {
     private Button bMenu = null;
 
+    private Button langageMain = null, time_ScoreMain = null, time_TipsMain = null, player_LevelMain = null;
+
+    private int langage;
+    private int time_Stockage_Score;
+    private int time_Stockage_Tips;
+    private int player_Level;
+
+    public void setLangage(int new_lang) {
+        langage = new_lang;
+        // 0 pour le français.
+        // 1 pour l'anglais
+    }
+
+    public void setTime_Stockage_Score(int time) {
+        // Si la valeur est à -1, alors le stockage est effectué indéfiniment.
+        time_Stockage_Score = time;
+    }
+
+    public void setTime_Stockage_Tips(int time) {
+        // Si la valeur est à -1, alors le stockage est effectué indéfiniment.
+        time_Stockage_Tips = time;
+    }
+
+    public void setPlayer_Level(int newLevel) {
+        player_Level = newLevel;
+    }
+
+    public int getLangage() {
+        return langage;
+        // 0 pour le français.
+        // 1 pour l'anglais
+    }
+
+    public int getTime_Stockage_Score() {
+        // Si la valeur est à -1, alors le stockage est effectué indéfiniment.
+        return time_Stockage_Score;
+    }
+
+    public int getTime_Stockage_Tips() {
+        // Si la valeur est à -1, alors le stockage est effectué indéfiniment.
+        return time_Stockage_Tips;
+    }
+
+    public int getPlayer_Level() {
+        return player_Level;
+    }
+
+
+
+    public void default_Initialisation() {
+        setLangage(0);
+        setTime_Stockage_Score(7);
+        setTime_Stockage_Tips(7);
+        setPlayer_Level(0);
+    }
+
     private View.OnClickListener clickListenerBoutons = new View.OnClickListener() {
         // private Intent deuxiemeActivite = null;
-
         @Override
         public void onClick(View v) {
             Intent activityTwo = null;
-            if(v.getId()==R.id.bouton_menu) {
-                activityTwo = new Intent(Option.this, Menu.class);
-                startActivity(activityTwo);
-            }
         }
     };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_option);
 
-        bMenu = (Button) findViewById(R.id.bouton_menu);
-        bMenu.setOnClickListener(clickListenerBoutons);
+        Button findMagicBtn1 = (Button) findViewById(R.id.magic_btn_langue);
+        findMagicBtn1.setOnClickListener(new View.OnClickListener() {
+            boolean animationClosing = false;
+            @Override
+            public void onClick(View v) {
+                if (!animationClosing){
+                    final LinearLayout findMagicLl = (LinearLayout) findViewById(R.id.magic_layout_langue);
+                    if (findMagicLl.getVisibility() == View.VISIBLE) {
+                        animationClosing = true;
+                        findMagicLl.animate().alpha(0f).setDuration(200).withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                findMagicLl.setVisibility(View.GONE);
+                                animationClosing = false;
+                            }
+                        }).start();
+                    } else {
+
+                        findMagicLl.setVisibility(View.VISIBLE);
+                        findMagicLl.setAlpha(1f);
+                    }
+                }
+
+            }
+        });
+
+        Button findMagicBtn2 = (Button) findViewById(R.id.magic_btn_temps);
+        findMagicBtn2.setOnClickListener(new View.OnClickListener() {
+            boolean animationClosing = false;
+            @Override
+            public void onClick(View v) {
+                if (!animationClosing){
+                    final LinearLayout findMagicLl = (LinearLayout) findViewById(R.id.magic_layout_temps);
+                    if (findMagicLl.getVisibility() == View.VISIBLE) {
+                        animationClosing = true;
+                        findMagicLl.animate().alpha(0f).setDuration(200).withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                findMagicLl.setVisibility(View.GONE);
+                                animationClosing = false;
+                            }
+                        }).start();
+                    } else {
+
+                        findMagicLl.setVisibility(View.VISIBLE);
+                        findMagicLl.setAlpha(1f);
+                    }
+                }
+
+            }
+        });
+
+        Button findMagicBtn3 = (Button) findViewById(R.id.magic_btn_level);
+        findMagicBtn3.setOnClickListener(new View.OnClickListener() {
+            boolean animationClosing = false;
+            @Override
+            public void onClick(View v) {
+                if (!animationClosing){
+                    final LinearLayout findMagicLl = (LinearLayout) findViewById(R.id.magic_layout_level);
+                    if (findMagicLl.getVisibility() == View.VISIBLE) {
+                        animationClosing = true;
+                        findMagicLl.animate().alpha(0f).setDuration(200).withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                findMagicLl.setVisibility(View.GONE);
+                                animationClosing = false;
+                            }
+                        }).start();
+                    } else {
+
+                        findMagicLl.setVisibility(View.VISIBLE);
+                        findMagicLl.setAlpha(1f);
+                    }
+                }
+
+            }
+        });
     }
-
-
-
-
-    //
-   // final ImageButton ireturn = (ImageButton) findViewById(R.id.iReturn);
-/*
-    ireturn.setOnClickListener(new View.OnClickListener() { //appelle les instruction dans OnClick ci-dessus quand cliqué
-        public void onClick(View v) {
-            Intent registerIntent = new Intent(Option.this, Menu.class);
-            Option.this.startActivity(registerIntent);   //ouvre une activité
-        }
-    }
-    );*/
 }
