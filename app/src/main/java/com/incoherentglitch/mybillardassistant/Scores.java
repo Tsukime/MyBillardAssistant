73775113c607713b383f600787c3afc1c241eb60
Tsukime <keip.priscilla@outlook.fr>
2017-04-06 10:15:59 +0200

    popup formulaire fonctionnelle

--- app/src/main/java/com/incoherentglitch/mybillardassistant/Scores.java
+++ app/src/main/java/com/incoherentglitch/mybillardassistant/Scores.java
@@ -2,6 +2,7 @@ package com.incoherentglitch.mybillardassistant;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -33,10 +34,7 @@ public class Scores extends Activity {
                 activityTwo = new Intent(Scores.this, Menu.class);
                 startActivity(activityTwo);
             }
-            /*if(v.getId()==R.id.bouton_nouveau)
-            {
-                Toast.makeText(Scores.this, "Add score pas encore implémenté D:", Toast.LENGTH_LONG).show();
-            }
+            /*
             if(v.getId()==R.id.bouton_recherche)
             {
                 Toast.makeText(Scores.this, "Recherche pas encore implémentée D:", Toast.LENGTH_LONG).show();
@@ -75,43 +73,71 @@ public class Scores extends Activity {
         barreRecherche = (EditText) findViewById(R.id.barre_recherche);
         //bFiltres = (Button) findViewById(R.id.bouton_selectFilters);
         //bRecherche = (Button) findViewById(R.id.bouton_recherche);
-        bNouveau = (ImageButton) findViewById(R.id.bouton_nouveau);
         bMenu = (Button) findViewById(R.id.bouton_menu_scores);
         bHelp = (Button) findViewById(R.id.bouton_help_scores);
 
         //barreRecherche.setOnClickListener(clickListenerBoutons);
         //bFiltres.setOnClickListener(clickListenerBoutons);
         //bRecherche.setOnClickListener(clickListenerBoutons);
+        bMenu.setOnClickListener(clickListenerBoutons);
+        bHelp.setOnClickListener(clickListenerBoutonHelp);
+
+        bNouveau = (ImageButton) findViewById(R.id.bouton_nouveau);
         bNouveau.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /* Find Tablelayout defined in main.xml */
-                TableLayout tl = (TableLayout) findViewById(R.id.tableau_scores);
-                /* Create a new row to be added. */
-                TableRow tr = new TableRow(Scores.this);
-                tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
-                /* Create a Button to be the row-content. */
-                TextView nom = new TextView(Scores.this);
-                TextView type = new TextView(Scores.this);
-                TextView date = new TextView(Scores.this);
-                ImageButton edit = new ImageButton(Scores.this);
-                ImageButton suppr = new ImageButton(Scores.this);
-                nom.setText("nom partie");
-                nom.setPadding(0, 20, 0, 0);
-                type.setText("type partie");
-                date.setText("date partie");
-                edit.setBackgroundResource(R.drawable.mini_crayon);
-                edit.setMaxWidth(20);
-                suppr.setBackgroundResource(R.drawable.mini_croix_rouge);
-                tr.addView(nom);
-                tr.addView(type);
-                tr.addView(date);
-                tr.addView(edit);
-                tr.addView(suppr);
-                tl.addView(tr, new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+                callLoginDialog();
+                //createScoreTableRow();
             }
         });
-        bMenu.setOnClickListener(clickListenerBoutons);
-        bHelp.setOnClickListener(clickListenerBoutonHelp);
+    }
+
+    private void createScoreTableRow(String nomP, String typeP){ //ajouter les parametres voulus
+        /* Find Tablelayout defined in main.xml */
+        TableLayout tl = (TableLayout) findViewById(R.id.tableau_scores);
+                /* Create a new row to be added. */
+        TableRow tr = new TableRow(Scores.this);
+        tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+                /* Create a Button to be the row-content. */
+        TextView nom = new TextView(Scores.this);
+        TextView type = new TextView(Scores.this);
+        TextView date = new TextView(Scores.this);
+        ImageButton edit = new ImageButton(Scores.this);
+        ImageButton suppr = new ImageButton(Scores.this);
+        nom.setText(nomP);
+        nom.setPadding(0, 20, 0, 0);
+        type.setText(typeP);
+        date.setText("date partie");
+        edit.setBackgroundResource(R.drawable.mini_crayon);
+        edit.setMaxWidth(20);
+        suppr.setBackgroundResource(R.drawable.mini_croix_rouge);
+        tr.addView(nom);
+        tr.addView(type);
+        tr.addView(date);
+        tr.addView(edit);
+        tr.addView(suppr);
+        tl.addView(tr, new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+    }
+
+    private void callLoginDialog()
+    {
+        final Dialog myDialog = new Dialog(Scores.this);
+        myDialog.setContentView(R.layout.create_score);
+        myDialog.setCancelable(true);
+        Button valider = (Button) myDialog.findViewById(R.id.partie_valider);
+
+        final EditText partieNom = (EditText) myDialog.findViewById(R.id.partie_nom);
+        final EditText partieType = (EditText) myDialog.findViewById(R.id.partie_type);
+        myDialog.show();
+
+
+        valider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createScoreTableRow(partieNom.getText().toString(), partieType.getText().toString());
+                myDialog.dismiss();
+            }
+        });
+
     }
 }
