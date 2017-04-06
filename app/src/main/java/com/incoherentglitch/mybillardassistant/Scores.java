9bacdfd0f3f23fe9c9fc62173574399b0f79dba7
Tsukime <keip.priscilla@outlook.fr>
2017-04-06 11:03:01 +0200

    ajout bordures table

--- app/src/main/java/com/incoherentglitch/mybillardassistant/Scores.java
+++ app/src/main/java/com/incoherentglitch/mybillardassistant/Scores.java
@@ -5,6 +5,9 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -15,6 +18,11 @@ import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 
 public class Scores extends Activity {
@@ -29,8 +37,7 @@ public class Scores extends Activity {
         @Override
         public void onClick(View v) {
             Intent activityTwo = null;
-            if(v.getId()==R.id.bouton_menu_scores)
-            {
+            if (v.getId() == R.id.bouton_menu_scores) {
                 activityTwo = new Intent(Scores.this, Menu.class);
                 startActivity(activityTwo);
             }
@@ -63,6 +70,11 @@ public class Scores extends Activity {
             dialog.show();
         }
     };
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -90,47 +102,90 @@ public class Scores extends Activity {
                 //createScoreTableRow();
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    private void createScoreTableRow(String nomP, String typeP){ //ajouter les parametres voulus
+    private void createScoreTableRow(String nomP, String typeP) { //ajouter les parametres voulus
         /* Find Tablelayout defined in main.xml */
         TableLayout tl = (TableLayout) findViewById(R.id.tableau_scores);
-                /* Create a new row to be added. */
         TableRow tr = new TableRow(Scores.this);
-        tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
-                /* Create a Button to be the row-content. */
+
+        TableLayout.LayoutParams tableRowParams =
+                new TableLayout.LayoutParams
+                        (TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT);
+
+        //tableRowParams.setMargins(5, 0, 5, 0); //left top right bottom
+
+        View ligneHorizontale1 = new View(this);
+        ligneHorizontale1.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, 1));
+        ligneHorizontale1.setBackgroundColor(Color.rgb(51, 51, 51));
+        View ligneHorizontale2 = new View(this);
+        ligneHorizontale2.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, 1));
+        ligneHorizontale2.setBackgroundColor(Color.rgb(51, 51, 51));
+
+        View ligneVerticale1 = new View(this);
+        ligneVerticale1.setLayoutParams(new TableRow.LayoutParams(1, TableRow.LayoutParams.FILL_PARENT));
+        ligneVerticale1.setBackgroundColor(Color.rgb(51, 51, 51));
+        View ligneVerticale2 = new View(this);
+        ligneVerticale2.setLayoutParams(new TableRow.LayoutParams(1, TableRow.LayoutParams.FILL_PARENT));
+        ligneVerticale2.setBackgroundColor(Color.rgb(51, 51, 51));
+        View ligneVerticale3 = new View(this);
+        ligneVerticale3.setLayoutParams(new TableRow.LayoutParams(1, TableRow.LayoutParams.FILL_PARENT));
+        ligneVerticale3.setBackgroundColor(Color.rgb(51, 51, 51));
+
         TextView nom = new TextView(Scores.this);
         TextView type = new TextView(Scores.this);
         TextView date = new TextView(Scores.this);
         ImageButton edit = new ImageButton(Scores.this);
         ImageButton suppr = new ImageButton(Scores.this);
         nom.setText(nomP);
-        nom.setPadding(0, 20, 0, 0);
+        nom.setPadding(10, 20, 10, 0);
         type.setText(typeP);
+        type.setPadding(10, 20, 10, 0);
         date.setText("date partie");
+        date.setPadding(10, 20, 10, 0);
+
         edit.setBackgroundResource(R.drawable.mini_crayon);
-        edit.setMaxWidth(20);
         suppr.setBackgroundResource(R.drawable.mini_croix_rouge);
+
+        tr.addView(ligneVerticale1);
         tr.addView(nom);
+        tr.addView(ligneVerticale2);
         tr.addView(type);
+        tr.addView(ligneVerticale3);
         tr.addView(date);
         tr.addView(edit);
         tr.addView(suppr);
-        tl.addView(tr, new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+
+        tl.addView(ligneHorizontale1);
+        tl.addView(tr, tableRowParams);
+        tl.addView(ligneHorizontale2);
     }
 
-    private void callLoginDialog()
-    {
+    private void callLoginDialog() {
         final Dialog myDialog = new Dialog(Scores.this);
         myDialog.setContentView(R.layout.create_score);
         myDialog.setCancelable(true);
         Button valider = (Button) myDialog.findViewById(R.id.partie_valider);
-
         final EditText partieNom = (EditText) myDialog.findViewById(R.id.partie_nom);
         final EditText partieType = (EditText) myDialog.findViewById(R.id.partie_type);
+
         myDialog.show();
 
-
+        partieNom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                partieNom.setText("");
+            }
+        });
+        partieType.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                partieType.setText("");
+            }
+        });
         valider.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -139,5 +194,41 @@ public class Scores extends Activity {
             }
         });
 
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Scores Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
