83d4918eac5435c5378e90ef377f91f5d132cbd4
Tsukime <keip.priscilla@outlook.fr>
2017-05-21 11:39:07 +0200

    cgu online

---
+++ app/src/main/java/com/incoherentglitch/mybillardassistant/CguView.java
@@ -0,0 +1,34 @@
+package com.incoherentglitch.mybillardassistant;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebView;
+
+public class CguView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView vue = new WebView(this);
+        String pdfUrl = "https://tsukime.github.io/cgu_v1.pdf";
+        String url = "https://docs.google.com/gview?embedded=true&url=" + pdfUrl;
+
+        vue.getSettings().setJavaScriptEnabled(true);
+
+        setContentView(vue);
+
+        vue.loadUrl(url);
+
+
+
+
+
+
+    }
+
+}
--- app/src/main/java/com/incoherentglitch/mybillardassistant/Option.java
+++ app/src/main/java/com/incoherentglitch/mybillardassistant/Option.java
@@ -1,16 +1,28 @@
 package com.incoherentglitch.mybillardassistant;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v4.content.FileProvider;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.LinearLayout;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static android.content.Intent.ACTION_VIEW;
 
 public class Option extends Activity {
     private Button bMenu = null;
 
     private Button langageMain = null, time_ScoreMain = null, time_TipsMain = null, player_LevelMain = null;
+    private Button cgu;
 
     private int langage;
     private int time_Stockage_Score;
@@ -18,9 +30,7 @@ public class Option extends Activity {
     private int player_Level;
 
     public void setLangage(int new_lang) {
-        langage = new_lang;
-        // 0 pour le français.
-        // 1 pour l'anglais
+        langage = new_lang; // 0 = français / 1 = anglais
     }
 
     public void setTime_Stockage_Score(int time) {
@@ -38,9 +48,7 @@ public class Option extends Activity {
     }
 
     public int getLangage() {
-        return langage;
-        // 0 pour le français.
-        // 1 pour l'anglais
+        return langage; // 0 = français / 1 = anglais
     }
 
     public int getTime_Stockage_Score() {
@@ -57,8 +65,6 @@ public class Option extends Activity {
         return player_Level;
     }
 
-
-
     public void default_Initialisation() {
         setLangage(0);
         setTime_Stockage_Score(7);
@@ -72,21 +78,6 @@ public class Option extends Activity {
 
     public void langage_English() {
 	setLangage(1);
-    }
-
-    public void set_times_to_Week() {
-	setTime_Stockage_Score(7);
-	setTime_Stockage_Tips(7);
-    }
-
-    public void set_times_to_Month() {
-	setTime_Stockage_Score(30);
-	setTime_Stockage_Tips(30);
-    }
-
-    public void set_times_to_Always() {
-	setTime_Stockage_Score(-1);
-	setTime_Stockage_Tips(-1);
     }
 
     public void set_player_level_to_Starter() {
@@ -113,6 +104,23 @@ public class Option extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option);
+
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+
+        final Context self = this;
+        cgu = (Button) findViewById(R.id.button_cgu);
+        cgu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(self, CguView.class);
+                startActivity(intent);
+
+            }
+        });
+
+
 
         Button findMagicBtn1 = (Button) findViewById(R.id.magic_btn_langue);
         findMagicBtn1.setOnClickListener(new View.OnClickListener() {
